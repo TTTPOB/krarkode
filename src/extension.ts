bf9e90af54ceca78ab1c7e29c882b90c969b0102
tpob <tpob@tpob.xyz>
2026-01-22 08:53:07 +0800

    feat: integrate ArkSidecarManager with SessionManager lifecycle

--- src/extension.ts
+++ src/extension.ts
@@ -3,17 +3,43 @@ import { setExtensionContext } from './context';
 import { ArkSessionManager } from './ark/sessionManager';
 import { ArkLanguageService } from './ark/arkLanguageService';
 import { CodeExecutor } from './ark/codeExecutor';
+import { ArkSidecarManager } from './ark/plotWatcher';
+import { ArkCommBackend } from './ark/arkCommBackend';
 import * as util from './util';
+import type { ArkSessionEntry } from './ark/sessionRegistry';
 
 let sessionManager: ArkSessionManager | undefined;
 let languageService: ArkLanguageService | undefined;
 let codeExecutor: CodeExecutor | undefined;
+let sidecarManager: ArkSidecarManager | undefined;
+let plotBackend: ArkCommBackend | undefined;
 
 export function activate(context: vscode.ExtensionContext): void {
     setExtensionContext(context);
     
+    // Create sidecar manager for plot/comm watching
+    sidecarManager = new ArkSidecarManager(
+        () => util.config().get<string>('krarkode.ark.sidecarPath') || 'ark-sidecar',
+        () => util.config().get<number>('krarkode.ark.sidecarTimeoutMs') ?? 30000
+    );
+    context.subscriptions.push(sidecarManager);
+    
+    // Create plot backend connected to sidecar
+    plotBackend = new ArkCommBackend(sidecarManager);
+    void plotBackend.connect();
+    context.subscriptions.push(plotBackend);
+    
     sessionManager = new ArkSessionManager();
     sessionManager.registerCommands(context);
+    
+    // When active session changes, attach sidecar to connection file
+    sessionManager.setActiveSessionHandler((entry: ArkSessionEntry | undefined) => {
+        if (entry && sidecarManager) {
+            sidecarManager.attach(entry.connectionFilePath);
+        } else if (sidecarManager) {
+            sidecarManager.stop();
+        }
+    });
     
     codeExecutor = new CodeExecutor();
     codeExecutor.registerCommands(context);
@@ -43,6 +69,10 @@ export function deactivate(): void {
     languageService = undefined;
     codeExecutor?.dispose();
     codeExecutor = undefined;
+    plotBackend?.dispose();
+    plotBackend = undefined;
+    sidecarManager?.dispose();
+    sidecarManager = undefined;
     sessionManager?.dispose();
     sessionManager = undefined;
 }
