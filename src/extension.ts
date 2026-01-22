307101988f50635c5289d44ee5463c60df79bbdb
tpob <tpob@tpob.xyz>
2026-01-22 08:48:33 +0800

    feat: integrate CodeExecutor into extension

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,21 @@ import * as vscode from 'vscode';
 import { setExtensionContext } from './context';
 import { ArkSessionManager } from './ark/sessionManager';
 import { ArkLanguageService } from './ark/arkLanguageService';
+import { CodeExecutor } from './ark/codeExecutor';
 import * as util from './util';
 
 let sessionManager: ArkSessionManager | undefined;
 let languageService: ArkLanguageService | undefined;
+let codeExecutor: CodeExecutor | undefined;
 
 export function activate(context: vscode.ExtensionContext): void {
     setExtensionContext(context);
     
     sessionManager = new ArkSessionManager();
     sessionManager.registerCommands(context);
+    
+    codeExecutor = new CodeExecutor();
+    codeExecutor.registerCommands(context);
     
     if (util.config().get<boolean>('krarkode.ark.lsp.enabled') ?? true) {
         // ArkLanguageService now auto-starts in constructor
@@ -29,12 +34,15 @@ export function activate(context: vscode.ExtensionContext): void {
         context.subscriptions.push(languageService);
     }
     
+    context.subscriptions.push(codeExecutor);
     context.subscriptions.push(sessionManager);
 }
 
 export function deactivate(): void {
     languageService?.dispose();
     languageService = undefined;
+    codeExecutor?.dispose();
+    codeExecutor = undefined;
     sessionManager?.dispose();
     sessionManager = undefined;
 }
