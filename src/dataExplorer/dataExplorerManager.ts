80641a9d694a3211f916d24956aa4d8e186c9167
tpob <tpob@tpob.xyz>
2026-01-23 14:21:10 +0800

    fix(data-explorer): allow codicon fonts in webview

--- src/dataExplorer/dataExplorerManager.ts
+++ src/dataExplorer/dataExplorerManager.ts
@@ -615,7 +615,7 @@ class DataExplorerPanel implements vscode.Disposable {
             <html lang="en">
             <head>
                 <meta charset="UTF-8">
-                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+                <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; font-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <link href="${styleUri}" rel="stylesheet">
                 <link href="${codiconUri}" rel="stylesheet">
