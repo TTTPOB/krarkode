7874cd8d81dec7474425cd4e83503e0fc46b5eb4
tpob <tpob@tpob.xyz>
2026-01-25 19:32:07 +0800

    test: add vscode extension test harness

---
+++ src/test/suite/index.ts
@@ -0,0 +1,19 @@
+import * as path from 'path';
+import Mocha from 'mocha';
+
+export function run(): Promise<void> {
+    const mocha = new Mocha({ ui: 'tdd', color: true });
+    const testsRoot = path.resolve(__dirname);
+
+    mocha.addFile(path.join(testsRoot, 'extension.test.js'));
+
+    return new Promise((resolve, reject) => {
+        mocha.run((failures: number) => {
+            if (failures > 0) {
+                reject(new Error(`${failures} tests failed.`));
+            } else {
+                resolve();
+            }
+        });
+    });
+}
