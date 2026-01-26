7d78a2ca3d55d90f7ae5f01f6523f910f938f1b8
tpob <tpob@tpob.xyz>
2026-01-26 13:41:58 +0800

    fix(data-explorer): export generated enums

--- src/dataExplorer/protocol.ts
+++ src/dataExplorer/protocol.ts
@@ -2,16 +2,30 @@ import type { ColumnProfileResult } from './protocol.generated';
 
 export * from './protocol.generated';
 
-export type {
+export {
     ColumnDisplayTypeElement as ColumnDisplayType,
-    ColumnFilterParamsObject as ColumnFilterParams,
     ColumnFilterTypeEnum as ColumnFilterType,
-    ColumnProfileSpecElement as ColumnProfileSpec,
     ColumnProfileTypeEnum as ColumnProfileType,
+    Condition as RowFilterCondition,
+    ExportFormatElement as ExportFormat,
+    Kind as TableSelectionKind,
+    Method as ColumnHistogramParamsMethod,
+    Op as FilterComparisonOp,
+    RowFilterTypeEnum as RowFilterType,
+    SortOrder as SearchSchemaSortOrder,
+    TextSearchTypeEnum as TextSearchType,
+} from './protocol.generated';
+
+export type {
+    ColumnFilterElement as ColumnFilter,
+    ColumnFilterParamsObject as ColumnFilterParams,
+    ColumnFilterTypeSupportStatusElement as ColumnFilterTypeSupportStatus,
+    ColumnProfileSpecElement as ColumnProfileSpec,
+    ColumnProfileTypeSupportStatusElement as ColumnProfileTypeSupportStatus,
     ColumnQuantileValueElement as ColumnQuantileValue,
     ColumnSortKeyElement as ColumnSortKey,
     CodeSyntaxNameResult as CodeSyntaxName,
-    Condition as RowFilterCondition,
+    ConvertToCode as ConvertToCodeFeatures,
     DataSelectionCellIndicesObject as DataSelectionCellIndices,
     DataSelectionCellRangeObject as DataSelectionCellRange,
     DataSelectionIndicesObject as DataSelectionIndices,
@@ -19,21 +33,23 @@ export type {
     DataSelectionSingleCellObject as DataSelectionSingleCell,
     DateStats as SummaryStatsDate,
     DatetimeStats as SummaryStatsDatetime,
-    ExportFormatElement as ExportFormat,
+    ExportDataSelection as ExportDataSelectionFeatures,
     FilterMatchDataTypesObject as FilterMatchDataTypes,
     FilterTextSearchObject as FilterTextSearch,
-    Kind as TableSelectionKind,
-    Method as ColumnHistogramParamsMethod,
+    GetColumnProfiles as GetColumnProfilesFeatures,
     NumberStats as SummaryStatsNumber,
     BooleanStats as SummaryStatsBoolean,
     OtherStats as SummaryStatsOther,
-    Op as FilterComparisonOp,
+    RowFilterElement as RowFilter,
     RowFilterParamsObject as RowFilterParams,
-    RowFilterTypeEnum as RowFilterType,
-    SortOrder as SearchSchemaSortOrder,
+    RowFilterTypeSupportStatusElement as RowFilterTypeSupportStatus,
+    SearchSchema as SearchSchemaFeatures,
+    SetColumnFilters as SetColumnFiltersFeatures,
+    SetRowFilters as SetRowFiltersFeatures,
+    SetSortColumns as SetSortColumnsFeatures,
     StringStats as SummaryStatsString,
     SummaryStats as ColumnSummaryStats,
-    TextSearchTypeEnum as TextSearchType,
+    TableSelectionObject as TableSelection,
 } from './protocol.generated';
 
 export type ColumnValue = number | string;
